d51755f538ce6f21ee670ed1d339bbb132eff1da
Christiaan van Zyl <christiaanvanzyl@gmail.com>
2016-05-05 17:38:14 +0200

    Fixed timetracker bug

--- vim_timetracker/timetracker.py
+++ vim_timetracker/timetracker.py
@@ -234,7 +234,7 @@ def tt_sum():
     rows_to_sum = []
     for l in range(cl - 1, 0, -1):
         if len(b[l]) > 0:
-            if b[l][0:4] == "#":
+            if b[l][0:2] == "# ":
                 # header = b[l - 1]
                 break
             else:
